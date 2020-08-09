3dd886ff22a49e378966ad170de678a3cfff39ca
rafaelgcaldas <rafaelzinhogomes22@live.com>
2020-08-09 00:58:49 -0300

    Integrate api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,50 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: string;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createOnConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/26827923?s=460&u=bd641998cc4763f2e8136ff48efd0745122536c2&v=4" alt="Rafael Gomes"/>
+                <img src={ teacher.avatar } alt={ teacher.name } />
                 <div>
-                    <strong>Rafael Gomes</strong>
-                    <span>Química</span>
+                    <strong>{ teacher.name }</strong>
+                    <span>{ teacher.subject }</span>
                 </div>
             </header>
 
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                <br /><br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </p>
+            <p>{ teacher.bio }</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 20,00</strong>
+                    <strong>R$ { teacher.cost }</strong>
                 </p>
-                <button type="button">
+                <a onClick={createOnConnection} href={`https://wa.me/${teacher.whatsapp}`} target="_blank">
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 function TeacherList() {
+    const [teachers, setTeachers] = useState([]);
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    async function SearchTeachers() {
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        });
+
+        setTeachers(response.data);
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis">
                 <form action="" id="search-teachers">
                 <Select
                         name="subject" 
-                        label="Matéria" 
+                        label="Matéria"
+                        value={subject}
+                        onChange={ event => setSubject(event.target.value) }
                         options={[
                             {value: 'Artes', label: 'Artes'},
                             {value: 'Biologia', label: 'Biologia'},
@@ -29,7 +50,9 @@ function TeacherList() {
                     />
                     <Select
                         name="week_day" 
-                        label="Dia da semana" 
+                        label="Dia da semana"
+                        value={week_day}
+                        onChange={ event => setWeekDay(event.target.value) }
                         options={[
                             {value: '0', label: 'Domingo'},
                             {value: '1', label: 'Segunda-feira'},
@@ -40,15 +63,24 @@ function TeacherList() {
                             {value: '6', label: 'Sábado'},
                         ]}
                     />
-                    <Input type="time" name="time" label="Hora" />
+                    <Input 
+                        type="time" 
+                        name="time" 
+                        label="Hora"
+                        value={time}
+                        onChange={ event => setTime(event.target.value) }
+                    />
+
+                    <button type="button" onClick={SearchTeachers}>Buscar</button>
                 </form>
             </PageHeader>
 
             <main>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {
+                    teachers.map((teacher: Teacher) => {
+                        return <TeacherItem key={teacher.id} teacher={ teacher } />
+                    })
+                }
             </main>
         </div>
     );
